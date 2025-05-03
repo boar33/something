a7a21bbc48d8c6b191c2d3f989c5d6ed601bdee0
boar33 <boarborg@gmail.com>
2025-05-03 13:23:54 -0600

    Add project files.

---
+++ Something/Triangle.cs
@@ -0,0 +1,24 @@
+﻿namespace Something
+{
+    internal class Triangle
+    {
+        private int _base;
+        private int _height;
+
+        public Triangle(int b, int height)
+        {
+            _base = b;
+            _height = height;
+        }
+
+        public int CalculateArea()
+        {
+            return (_base * _height) / 2;
+        }
+
+        public string AsString()
+        {
+            return $"Base is {_base}, height is {_height}";
+        }
+    }
+}
