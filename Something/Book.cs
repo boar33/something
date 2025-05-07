db84d2652e99925978a5cdb369339cb3e0659e4d
boar33 <boarborg@gmail.com>
2025-05-06 20:28:37 -0600

    add book class

---
+++ Something/Book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Something;
+
+public class Book
+{
+    public string Title;
+    public string Author;
+    public string Publisher;
+    public int Pages;
+    public string Description;
+}
